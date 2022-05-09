9a6e065878f3785f7cf910589bc64b84abdc5f27
sanskar <sanskarraj19072001@gmail.com>
2022-05-09 23:03:54 +0530

    final updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Firstwarning from './components/firstwarning';
 import Landing from './components/landing';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 function App() {
 
-  const[check,Setcheck] =useState(!true)
+  const[check,Setcheck] =useState(true)
+  
+  useEffect(() => {
+  const timer = setTimeout(() => 
+  Setcheck(false), 12000);
+}, []);
+
   return (
     <div className="App">
       {check ? <Firstwarning/> : <Landing/>}
